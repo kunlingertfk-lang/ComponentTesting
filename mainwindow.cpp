d017fe6f3b798ef2136a4a11a001da2264252e89
kunling <2858309122@qq.com>
2026-03-25 16:04:24 +0800

    Revert "测试修改分支revert"
    This reverts commit 799c924871ec15f665f2d674070cc2458f92e262.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include <QDebug>
-
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    qDebug() << "bbbb";
 }
 
 MainWindow::~MainWindow()
